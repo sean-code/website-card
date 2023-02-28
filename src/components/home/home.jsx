aa9818846fa3fad267e8130404f46299e3c38864
sean-code <john.nganga@student.moringaschool.com>
2023-02-28 03:01:49 +0300

    style based on  color choice

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,7 +1,14 @@
 import './home.css';
 import Profile from './assets/img/profile.jpg';
+import Github from './assets/img/github.svg';
+import Instagram from './assets/img/instagram.svg';
+import Linked from './assets/img/linkedin.svg';
+import Map from './assets/img/map-pin.svg';
+import Twitter from './assets/img/twitter.svg';
+import Mail from './assets/img/mail.svg'
 
-const Home = () =>{
+
+const Home =() =>{
     return(
         <>
         <div className="wrapper">
@@ -12,23 +19,23 @@ const Home = () =>{
 							<img src={Profile} />
 						</div>
 						<div className="box-inner">
-							<h3 className="name">John Sean Nganga</h3>
-							<p>Hey there, welcome!</p>
-							<p>My name is Nganga Sean</p>
-							<h4 className="occupation"> - A boy who met Tech - </h4>
+							<h3 className="name" style={{fontSize:'2.0rem', marginTop:'10px', fontWeight:'800'}}>Hey, there! I'm Sean</h3>
+							<p style={{fontSize:'1.4rem'}}> A tech enthusiast who curates experiences with technology</p>
+
+							<h4 className="occupation">  I am a Software Engineer, UI/UX Designer, a BioEngineering Graduate, and an IT student. </h4>
 							<p className="location">
 								<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjzsKXY4pn7AhWK_7sIHVwdDXUQFnoECCkQAQ&url=https%3A%2F%2Fwww.google.com%2Fmymaps%2Fviewer%3Fmid%3D1s1yc_gbRmShBhwg2XYxQlDaj_UA%26hl%3Den_GB&usg=AOvVaw2OHijzogKcup12p_5XW0K0" target="_blank">
-									<img src="" height="20px" />
+									<img src={Map} height="20px" />
 									Nairobi, KE
 								</a>
 							</p>
-							<h3 className="followers-title">Stalk Me Below:</h3>
+							<h3 className="followers-title">Stalk My Handles:</h3>
 							<ul className="followers-list">
-								<li><a href='https://www.instagram.com/its_ngangasean' target='_blank'><img src="./img/instagram.svg" alt="" /></a></li>
-								<li><a href='https://www.twitter.com/nganga_sea' target='_blank'><img src="./img/twitter.svg" alt="" /></a></li>
-								<li><a href="https://www.linkedin.com/in/john-sean-nganga/" target='_blank'><img src="./img/linkedin.svg" alt="" /></a></li>
-								<li><a href="https://t.me/sean_code" target='_blank'><img src="./img/send.svg" alt="" /></a></li>
-								<li><a href="https://github.com/sean-code" target='_blank'><img src="./img/github.svg" alt="" /></a></li>
+								<li><a href='https://www.instagram.com/its_ngangasean' target='_blank'><img src={Instagram} alt="" /></a></li>
+								<li><a href='https://www.twitter.com/nganga_sea' target='_blank'><img src={Twitter} alt="" /></a></li>
+								<li><a href="https://www.linkedin.com/in/john-sean-nganga/" target='_blank'><img src={Linked}alt="" /></a></li>
+								<li><a href="https://t.me/sean_code" target='_blank'><img src={Mail} alt="" /></a></li>
+								<li><a href="https://github.com/sean-code" target='_blank'><img src={Github} alt="" /></a></li>
 							</ul>
 						</div>
 					</div>
