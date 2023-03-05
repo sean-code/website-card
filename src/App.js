01c398502ce8a19fa02a4588838b335cde9f780e
sean-code <john.nganga@student.moringaschool.com>
2023-03-06 01:08:16 +0300

    Ipdate routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NavBar from './components/navbar/navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/home/home';
+import About from './components/about/about';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path='/' element={<Home />}></Route>
-
+            <Route path='/about' element={<About />}></Route>
           </Routes>
         </BrowserRouter>
     </div>
